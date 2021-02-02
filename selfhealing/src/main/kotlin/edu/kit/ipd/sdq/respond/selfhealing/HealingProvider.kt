cd6f970ff50032c4e1977b01f7261fc3b7d072c6
Adrian Freund <adrian@freund.io>
2021-02-02 01:00:43 +0100

    Change selfhealing default return

--- selfhealing/src/main/kotlin/edu/kit/ipd/sdq/respond/selfhealing/HealingProvider.kt
+++ selfhealing/src/main/kotlin/edu/kit/ipd/sdq/respond/selfhealing/HealingProvider.kt
@@ -10,7 +10,7 @@ class HealingProvider {
 
     private fun defaultPayload(id: UUID) = HealingPayload(
         ProcessPayload(
-            DefinitionIdPayload(UUID(0, 0), "1.0")
+            DefinitionIdPayload(UUID.fromString("15c81df0-509c-4459-b7d2-485c9f69f5be"), "1.0")
         ),
         TaskPayload(id)
     )
